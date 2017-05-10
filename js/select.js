7a56cf87d6b1a836a8a8a37879a82dae89f492a6
meishadevs <meishadevs@163.com>
2017-05-10 10:04:19 +0800

    商品筛选功能完成

--- js/select.js
+++ js/select.js
@@ -7,15 +7,32 @@ $(function () {
 
     //隐藏筛选项
     hideFilterItem();
+
+    //显示筛选项
+    showFilterItem();
 });
+
 
 //隐藏筛选项
 function hideFilterItem() {
     $(".screeningBody a").on("click", function () {
         var className = $(this).attr("data");
         var position = $(this).attr("position");
+        console.log("className:", className);
+        console.log("position:", position);
+        console.log(this.innerHTML);
         $("." + className).hide();
         $("." + position).show();
         $("." + position + "-text").html(this.innerHTML);
+    });
+}
+
+
+//显示筛选项
+function showFilterItem() {
+    $(".userPosition a").on("click", function () {
+        $(this).parent().hide();
+        var className = $(this).attr("data-class");
+        $("." + className).show();
     });
 }
