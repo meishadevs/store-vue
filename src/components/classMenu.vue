c97aad059ad129a8d34acdea47b26c12e7411270
meishadevs <meishadevs@163.com>
2017-11-28 12:14:14 +0800

    商品分类页制作完成

---
+++ src/components/classMenu.vue
@@ -0,0 +1,169 @@
+<!-- 分类菜单组件 -->
+
+<template>
+  <div class="class-menu">
+    <h3 class="product-title">
+      <a href="javascript:;" v-for="title in classInfo.headTitle">{{ title }}、</a>
+    </h3>
+    <dl class="product-item" v-for="item in classInfo.classList">
+      <dt>{{ item.classTitle }}</dt>
+      <dd>
+        <a href="javascript:;" v-for="content in item.classContent">{{ content }}</a>
+      </dd>
+    </dl>
+  </div>
+</template>
+
+<script>
+  export default {
+
+    //组件名称
+    name: 'classMenu',
+
+    data() {
+      return {
+
+        //商品分类信息
+        classInfo: {
+          'headTitle': ['手机', '相机', '数码'],
+          'classList': [
+            {
+              'classTitle': '手机通讯',
+              'classContent': [
+                '全部手机',
+                '1080P高清',
+                '双卡手机',
+                '拍照神器',
+                '老年手机',
+                '对讲机',
+                '联通专区'
+              ]
+            },
+            {
+              'classTitle': '数码相机',
+              'classContent': [
+                '卡片机',
+                '中长焦',
+                '专业便',
+                '携自拍神器',
+                '防水防摔',
+                '一次成像'
+              ]
+            },
+            {
+              'classTitle': '单反',
+              'classContent': [
+                '全部',
+                '佳能',
+                '尼康',
+                '宾得',
+                '哈苏'
+              ]
+            },
+            {
+              'classTitle': '镜头',
+              'classContent': [
+                '单反镜头',
+                '微单镜头'
+              ]
+            },
+            {
+              'classTitle': '摄像机',
+              'classContent': [
+                '全部',
+                '索尼',
+                '松下',
+                '佳能',
+                '三星'
+              ]
+            },
+            {
+              'classTitle': '摄像配件',
+              'classContent': [
+                '相机包',
+                '相机电池',
+                '贴膜',
+                '滤镜',
+                '三脚架',
+                '闪光灯',
+                '遮光器',
+                '遥控器',
+                '专业配件',
+                '清洁用品',
+                '望远镜'
+              ]
+            },
+            {
+              'classTitle': '数码存储',
+              'classContent': [
+                '刻录盘片',
+                '盘片附件'
+              ]
+            },
+            {
+              'classTitle': '电子教育',
+              'classContent': [
+                '电子书',
+                '盒电子词典',
+                '幼儿教育',
+                '智能学习',
+                '录音笔'
+              ]
+            }
+          ]
+        }
+      };
+    }
+  };
+</script>
+
+<style scoped>
+  .class-menu {
+    width: 183px;
+    border: solid 1px #d7d7d7;
+    background-color: #f3f3f3;
+  }
+
+  .class-menu .product-title {
+    height: 34px;
+    font: bold 14px/34px "宋体";
+    text-align: center;
+    border-bottom: solid 1px #cdcdcd;
+  }
+
+  .class-menu .product-title a {
+    color: #555;
+  }
+
+  .class-menu .product-item {
+    padding: 10px 0 0 20px;
+    border-bottom: dotted 1px #cdcdcd;
+  }
+
+  .class-menu .product-item dt {
+    height: 30px;
+    font-family: "宋体";
+    font-weight: bold;
+    color: #555;
+  }
+
+  .class-menu .product-item dd {
+    font-size: 0;
+    font-family: "宋体";
+  }
+  .class-menu .product-item dd a {
+    width: 81px;
+    height: 25px;
+    font-size: 12px;
+    color: #555;
+    display: inline-block;
+  }
+
+  .product-item dd a:hover {
+    color: #c81623;
+  }
+
+  .class-menu dl:nth-last-child(1){
+    border-bottom: none;
+  }
+</style>
