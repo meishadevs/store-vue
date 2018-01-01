85e3f471780a15082820e25e2cfc33fb3b2606ff
meishadevs <meishadevs@163.com>
2018-01-01 12:45:39 +0800

    修改了项目发布时的目录

--- config/index.js
+++ config/index.js
@@ -53,10 +53,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../dswz/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../dswz'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
 
