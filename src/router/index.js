b326c186dc1cee26a209cdbd1c7dbdbf1e75b123
meishadevs <meishadevs@163.com>
2018-02-04 09:27:35 +0800

    修改了组件的命名

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import index from "@/pages/index";
+import home from "@/pages/home";
 import cate from "@/pages/cate";
 import select from "@/pages/select";
 import proinfo from "@/pages/proinfo";
@@ -14,8 +14,8 @@ export default new VueRouter({
   routes: [
     {
       path: "/",
-      name: "index",
-      component: index
+      name: "home",
+      component: home
     },
     {
       path: "/cate",
