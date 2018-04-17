40cafdbce6b8d1a50d6a74dd4d0a2974d2388191
meishadevs <meishadevs@gmail.com>
2018-04-17 11:40:04 +0800

    使用路由预加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,56 +1,50 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import home from "@/pages/home";
-import cate from "@/pages/cate";
-import choose from "@/pages/choose";
-import proinfo from "@/pages/proinfo";
-import cart from "@/pages/cart";
-import register from "@/pages/register";
-import login from "@/pages/login";
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
   routes: [
     {
+      //跳转到网站首页的路由
       path: "/",
-      name: "home",
-      component: home
+      component: r => require.ensure([], () => r(require("../pages/home")), "home")
     },
     {
+      //跳转到商品分类页的路由
       path: "/cate",
-      name: "cate",
-      component: cate
+      component: r => require.ensure([], () => r(require("../pages/cate")), "cate")
     },
     {
+      //跳转到商品选择页的路由
       path: "/choose",
-      name: "choose",
-      component: choose
+      component: r => require.ensure([], () => r(require("../pages/choose")), "choose")
     },
     {
+      //跳转到商品信息展示页的路由
       path: "/proinfo",
-      name: "proinfp",
-      component: proinfo
+      component: r => require.ensure([], () => r(require("../pages/proinfo")), "proinfo")
     },
     {
+      //跳转到购物车页的路由
       path: "/cart",
-      name: "cart",
-      component: cart
+      component: r => require.ensure([], () => r(require("../pages/cart")), "cart")
     },
     {
+      //跳转到注册页的路由
       path: "/register",
-      name: "register",
-      component: register
+      component: r => require.ensure([], () => r(require("../pages/register")), "register")
     },
     {
+      //跳转到登录页的路由
       path: "/login",
-      name: "login",
-      component: login
+      component: r => require.ensure([], () => r(require("../pages/login")), "login")
     },
     {
+      //当用户点击加入购物车按钮时判断用户是否登录了
+      //当用户没有登录时通过这个路由跳转到登录页，在登录页登录成功后直接跳转到购物车页
       path: "/login/:isCart",
-      name: cart,
-      component: login
+      component: r => require.ensure([], () => r(require("../pages/login")), "login")
     }
   ]
 });
