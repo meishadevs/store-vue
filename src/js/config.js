5ca877b490d97b2ea1331de0d2d250770c8534bc
meishadevs <meishadevs@gmail.com>
2018-05-13 10:56:53 +0800

    修改了服务器端的url

--- src/js/config.js
+++ src/js/config.js
@@ -6,7 +6,7 @@ export default {
   install(Vue, option) {
 
     //服务器端的url
-    Vue.prototype.serverUrl = 'http://meishadevs.sc2yun.com/dswzServer/';
+    Vue.prototype.serverUrl = 'http://localhost/dswzServer/';
 
     //获得省、市、区数据的url
     Vue.prototype.addressUrl = Vue.prototype.serverUrl + 'place.php';
