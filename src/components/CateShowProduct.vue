0a47f328bb15b4ed1e436552bacd8622bcad7938
meishadevs <meishadevs@163.com>
2022-09-23 15:37:54 +0800

    解决数码影音和娱乐影音下的图片没显示的问题

--- src/components/CateShowProduct.vue
+++ src/components/CateShowProduct.vue
@@ -7,10 +7,14 @@
       <h3>{{ productList.title }}</h3>
     </div>
     <ul class="product-list clearfix">
-      <li class="product-item" v-for="(product, index) in productList.content" :key="index">
+      <li
+        class="product-item"
+        v-for="(product, index) in productList.content"
+        :key="index"
+      >
         <div class="product-image">
           <router-link to="/proinfo">
-            <img :src="product.image" alt="商品">
+            <img :src="require(`@/assets/images/${product.image}`)" alt="商品" />
           </router-link>
         </div>
         <div class="product-name">
@@ -19,8 +23,15 @@
         <p class="product-price">￥{{ product.price }}</p>
         <p class="product-comment">
           评论:
-          <span class="star" :style="{width: product.numStar * 13 + 'px'}"></span>
-          <span class="comment-num" href="javascript:;">({{ product.numComment }}条)</span>
+          <span
+            class="star"
+            :style="{ width: product.numStar * 13 + 'px' }"
+          ></span>
+          <span
+class="comment-num"
+href="javascript:;"
+            >({{ product.numComment }}条)</span
+          >
         </p>
       </li>
     </ul>
@@ -29,7 +40,6 @@
 
 <script>
 export default {
-
   // 组件名称
   name: 'CateShowProduct',
 
@@ -39,56 +49,56 @@ export default {
 </script>
 
 <style lang="less" scoped>
-  .cate-show-product .cate-name {
-    height: 34px;
-    border-bottom: solid 2px #ccc;
-  }
+.cate-show-product .cate-name {
+  height: 34px;
+  border-bottom: solid 2px #ccc;
+}
 
-  .cate-name h3 {
-    width: 180px;
-    height: 34px;
-    padding-left: 10px;
-    border-bottom: solid 2px #096dba;
-    font: bold 14px/34px "宋体", "SimSun";
-  }
+.cate-name h3 {
+  width: 180px;
+  height: 34px;
+  padding-left: 10px;
+  border-bottom: solid 2px #096dba;
+  font: bold 14px/34px "宋体", "SimSun";
+}
 
-  .product-list {
-    width: 790px;
-    margin-left: 16px;
-  }
+.product-list {
+  width: 790px;
+  margin-left: 16px;
+}
 
-  .product-item {
-    margin: 18px 32px 30px 0;
-    float: left;
-  }
+.product-item {
+  margin: 18px 32px 30px 0;
+  float: left;
+}
 
-  .product-item .product-name {
-    width: 162px;
-    height: 50px;
-  }
+.product-item .product-name {
+  width: 162px;
+  height: 50px;
+}
 
-  .product-item .product-name a {
-    color: #333333;
-  }
+.product-item .product-name a {
+  color: #333333;
+}
 
-  .product-item .product-name a:hover {
-    color: #DD0000;
-  }
+.product-item .product-name a:hover {
+  color: #dd0000;
+}
 
-  .product-item .product-price {
-    height: 20px;
-    color: #d4105a;
-    font-weight: bold;
-  }
+.product-item .product-price {
+  height: 20px;
+  color: #d4105a;
+  font-weight: bold;
+}
 
-  .product-item .product-comment .star {
-    width: 13px;
-    height: 12px;
-    background: url("~@/assets/images/star.png");
-    display: inline-block;
-  }
+.product-item .product-comment .star {
+  width: 13px;
+  height: 12px;
+  background: url("~@/assets/images/star.png");
+  display: inline-block;
+}
 
-  .product-item .product-comment .comment-num {
-    color: #085c9b;
-  }
+.product-item .product-comment .comment-num {
+  color: #085c9b;
+}
 </style>
