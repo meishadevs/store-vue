b230cfd34b336e2f6348a3895862257e4adb6b58
meishadevs <meishadevs@gmail.com>
2022-09-30 11:33:35 +0800

    将样式改成 less 的写法

--- src/components/AdvisoryList.vue
+++ src/components/AdvisoryList.vue
@@ -1,15 +1,20 @@
-
-<!-- 商品咨询列表组件 -->
-
 <template>
   <ul class="advisory-list">
-    <li class="clearfix" v-for="(advisory, index) in advisoryListData" :key="index">
+    <li
+      class="clearfix"
+      v-for="(advisory, index) in advisoryListData"
+      :key="index"
+    >
       <div class="member-info">
         <div class="member-image">
-          <img :src="serverUrl + 'images/advisoryimages/' + advisory.memberimage" alt="">
+          <img
+            :src="serverUrl + 'images/advisoryimages/' + advisory.memberimage"
+            alt=""
+          />
         </div>
         <p class="member-name">{{ advisory.membernum }}</p>
-        <p class="member-grade">{{ advisory.membergrade }}</p></div>
+        <p class="member-grade">{{ advisory.membergrade }}</p>
+      </div>
       <div class="shop-advisory">
         <div class="top clearfix">
           <div class="advisory-name">[商品咨询]</div>
@@ -29,8 +34,6 @@
 
 <script>
 export default {
-
-  // 组件名称
   name: 'AdvisoryList',
 
   // 获取从父组件中传递过来的数据
@@ -38,14 +41,13 @@ export default {
 
   data() {
     return {
-
       // 商品咨询列表
       advisoryListData: [],
 
       // 每页第一条商品咨询信息的下标
       advisoryFirstIndex: 0,
 
-      // 当前展示的是第indexPage页商品咨询信息
+      // 当前展示的是第 indexPage 页商品咨询信息
       indexPage: 0
     };
   },
@@ -69,8 +71,6 @@ export default {
 
   // 监听器
   watch: {
-
-    // 如果indexPage发生改变，这个函数就会调用
     indexPage() {
       // 获得商品咨询信息
       this.getAdvisoryInfo();
@@ -78,118 +78,122 @@ export default {
   },
 
   methods: {
-
     // 获得商品咨询信息
     getAdvisoryInfo() {
       // 计算每页展示的第一条商品咨询信息的下标
       this.advisoryFirstIndex = (this.indexPage - 1) * this.numAdvisory;
 
       // 发送get请求，获得商品咨询信息
-      this.$jsonp(this.advisoryInfoUrl + this.numAdvisory + '&startIndex=' + this.advisoryFirstIndex, null, (err, data) => {
-        if (err) {
-          console.error('error:', err.message);
-        } else {
-          // 获得商品咨询列表数据
-          this.advisoryListData = data;
+      this.$jsonp(this.advisoryInfoUrl + this.numAdvisory + '&startIndex=' + this.advisoryFirstIndex, null,
+        (err, data) => {
+          if (err) {
+            console.error('error:', err.message);
+          } else {
+            // 获得商品咨询列表数据
+            this.advisoryListData = data;
+          }
         }
-      });
+      );
     }
   }
 };
 </script>
 
 <style lang="less" scoped>
+// 咨询列表
+.advisory-list {
+  margin-left: 67px;
 
-  /* 咨询列表 */
-  .advisory-list {
-    margin-left: 67px;
-  }
-
-  .advisory-list li {
+  li {
     margin-bottom: 38px;
   }
+}
 
-  /* 会员信息 */
-  .member-info {
-    text-align: center;
-    float: left;
-  }
+// 会员信息
+.member-info {
+  text-align: center;
+  float: left;
+}
 
-  /* 会员名称 */
-  .member-name, .member-grade {
-    height: 18px;
-    color: #777777;
-  }
+.member-name,
+.member-grade {
+  height: 18px;
+  color: #777777;
+}
 
-  /* 商品咨询 */
-  .shop-advisory {
-    margin-left: 34px;
-    float: left;
-  }
+// 商品咨询
+.shop-advisory {
+  margin-left: 34px;
+  float: left;
 
-  .shop-advisory .top {
+  .top {
     width: 540px;
     height: 19px;
     line-height: 19px;
     margin-bottom: 15px;
   }
+}
 
-  .member-image img {
+.member-image {
+  img {
     width: 35px;
     height: 35px;
     border-radius: 15px;
     display: inline-block;
   }
+}
 
-  .advisory-name {
-    font-size: 14px;
-    color: #000;
-    font-weight: bold;
-    float: left;
-  }
+.advisory-name {
+  font-size: 14px;
+  color: #000;
+  font-weight: bold;
+  float: left;
+}
 
-  /* 咨询的时间 */
-  .advisory-time {
-    font-family: "宋体", "SimSun";
-    color: #ABABAB;
-    float: right;
-  }
+// 咨询的时间
+.advisory-time {
+  font-family: "宋体", "SimSun";
+  color: #ababab;
+  float: right;
+}
 
-  /* 提问 */
-  .advisory-content .question {
+// 提问
+.advisory-content {
+  .question {
     margin-bottom: 13px;
     font-family: "宋体", "SimSun";
     color: #777777;
   }
+}
 
-  /* 回答 */
-  .answer {
-    width: 576px;
-    padding-left: 30px;
-    padding-top: 20px;
-    padding-bottom: 20px;
-    line-height: 20px;
-    font-family: "宋体", "SimSun";
-    font-size: 12px;
-    background-color: #F1F1F1;
-    color: #777777;
-    position: relative;
-  }
+// 回答
+.answer {
+  width: 576px;
+  padding-left: 30px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  line-height: 20px;
+  font-family: "宋体", "SimSun";
+  font-size: 12px;
+  background-color: #f1f1f1;
+  color: #777777;
+  position: relative;
 
-  .answer span {
+  span {
     color: #000000;
     font-weight: bold;
   }
 
-  /* 小三角 */
-  .answer .triangle {
+  // 小三角
+  .triangle {
     width: 0;
     height: 0;
     border-style: solid;
-    border-color: transparent transparent #F3F3F3 transparent;
+    border-color: transparent transparent #f3f3f3 transparent;
     border-width: 0 5px 5px 5px;
     position: absolute;
     top: -5px;
     left: 30px;
   }
+}
 </style>
